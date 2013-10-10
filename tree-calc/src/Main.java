f56cc4a97b8883af0eec32247b57ecd37817c4d4
Sam Berry <sberry925@gmail.com>
2013-10-10 17:05:26 -0400

    Activated Guava and added a stopwatch stub

---
+++ tree-calc/src/Main.java
@@ -0,0 +1,19 @@
+import com.google.common.base.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author berrysa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        for (int i = 0; i < 1000000000; i++) {
+            i++;
+        }
+
+        System.out.println("Time: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    }
+}
