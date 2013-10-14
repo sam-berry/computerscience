19cf04da0a1d9838cee92dbd6bb2df8faaea5a39
Sam Berry <sberry925@gmail.com>
2013-10-14 11:58:17 -0400

    Adding a todo per Erik's suggestion

--- tree-calc/src/Main.java
+++ tree-calc/src/Main.java
@@ -7,6 +7,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
 
+    // todo: we should clean up the project so the jdk isn't committed and recreate the project to be file-based instead of directory based and put the project file in /computerscience instead of /computerscience/stack-calc
+
+
     public static void main(String[] args) {
         Stopwatch stopwatch = Stopwatch.createStarted();
 
